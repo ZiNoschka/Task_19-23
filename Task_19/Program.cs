e76a2d67df11ecf61aa1273a12e46b5c0d5efb71
Zina <zn26941@gmail.com>
2023-07-17 12:08:19 +0300

    изменение

---
+++ Task_19/Program.cs
@@ -0,0 +1,20 @@
+﻿{
+   Console.Write("Введите пятизначное число: ");
+        int number = int.Parse(Console.ReadLine());
+
+        int digit1 = number / 10000;
+        int digit2 = (number / 1000) % 10;
+        int digit3 = (number / 100) % 10;
+        int digit4 = (number / 10) % 10;
+        int digit5 = number % 10;
+
+        if (digit1 == digit5 && digit2 == digit4)
+        {
+            Console.WriteLine("Число является палиндромом.");
+        }
+        else
+        {
+            Console.WriteLine("Число не является палиндромом.");
+        }
+    
+}
