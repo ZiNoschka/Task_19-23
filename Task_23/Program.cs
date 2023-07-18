b7b774222b16e40a6cf2890448b3e5dfb020a6b6
Zina <zn26941@gmail.com>
2023-07-18 17:05:50 +0300

    добавила программу

--- Task_23/Program.cs
+++ Task_23/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿{
+    static void Main(string[] args)
+    {
+        Console.Write("Введите число N: ");
+        int n = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Таблица кубов чисел от 1 до N:");
+        Console.WriteLine("Число\tКуб");
+
+        for (int i = 1; i <= n; i++)
+        {
+            int cube = i * i * i;
+            Console.WriteLine($"{i}\t{cube}");
+        }
+    }
+}
